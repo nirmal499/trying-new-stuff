519a34d9ce42412beb7ab917ea261fd146f42e7d
nirmal499 <nbaskey499@gmail.com>
2021-10-20 19:30:11 +0530

    Updated to support two way Transpilation

--- typescriptW/run.sh
+++ typescriptW/run.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/bash
-flag1="--lib es6,dom"
+#########################################################################################
+# To run : ./run.sh <file_name_without_extension> <way[either_1[Default]_or_anyNumber]>
+#########################################################################################
+# flag1="--lib es6,dom"
+# flag1="--lib es2018,dom"
+flag1="--lib es2020,dom"
 flag2="--experimentalDecorators"
 flag3="--target es2015"
 flag4="--emitDecoratorMetadata"
-tsc ${1}.ts ${flag1} ${flag2} ${flag3} ${flag4} && node ${1}.js
+
+defaultWayValue=1
+way=${2:-${defaultWayValue}}
+
+if [[ ${way} == 1 ]]; then
+    tsc ${1}.ts ${flag1} ${flag2} ${flag3} ${flag4} && node ${1}.js
+else
+    tsc ${1}.ts ${flag1} ${flag2} ${flag3} ${flag4}
+fi
+
 # This is not working for parameterDecorator
